f589a9cab33a4db1ba6f041916f44f6e5730ffd2
Leandro Perin <leandro.perino@gmail.com>
2018-06-09 02:58:56 -0300

    First version of FIRST

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,44 @@
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		// S->aSb|ab
-		
 		Symbol S = new Symbol("S", Symbol.Type.NON_TERMINAL);
+		Symbol B = new Symbol("B", Symbol.Type.NON_TERMINAL);
+		Symbol A = new Symbol("A", Symbol.Type.NON_TERMINAL);
+		
 		Symbol a = new Symbol("a", Symbol.Type.TERMINAL);
 		Symbol b = new Symbol("b", Symbol.Type.TERMINAL);
+		Symbol c = new Symbol("c", Symbol.Type.TERMINAL);
+		Symbol d = new Symbol("d", Symbol.Type.TERMINAL);
+		Symbol epsilon = new Symbol("&", Symbol.Type.TERMINAL);
 		
-		Symbol[] s1 = {a, S, b};
-		Symbol[] s2 = {a, b};
+		Symbol[] s0 = {A, b};
+		Symbol[] s1 = {A, B, c};
+		Symbol[] b0 = {b, B};
+		Symbol[] b1 = {A, d};
+		Symbol[] b2 = {epsilon};
+		Symbol[] a0 = {a, A};
+		Symbol[] a1 = {epsilon};
 		
+		Production p0 = new Production(S, s0);
 		Production p1 = new Production(S, s1);
-		Production p2 = new Production(S, s2);
+		Production p2 = new Production(B, b0);
+		Production p3 = new Production(B, b1);
+		Production p4 = new Production(B, b2);
+		Production p5 = new Production(A, a0);
+		Production p6 = new Production(A, a1);
 
 		Grammar G = new Grammar();
+		G.addProduction(p0);
 		G.addProduction(p1);
 		G.addProduction(p2);
+		G.addProduction(p3);
+		G.addProduction(p4);
+		G.addProduction(p5);
+		G.addProduction(p6);
 		
-		System.out.println(G.toString());
+		for (Symbol s: S.getFirst()) {
+			System.out.print(s.toString());
+		}
 	}
 
 }
--- src/Symbol.java
+++ src/Symbol.java
@@ -10,9 +10,11 @@ public class Symbol {
 	private Type type;
 	private String name;
 	private Set<Production> productionsTo = new HashSet<Production>();
+	private Set<Symbol> first = new HashSet<Symbol>();
+	private Set<Symbol> follow = new HashSet<Symbol>();
 	
 	/*
-	 * Creates a new NonTerminalSymbol
+	 * Creates a new Symbol
 	 * */
 	public Symbol(String _name, Type _type) throws Exception {
 		if (_name.length() == 1) {
@@ -21,6 +23,63 @@ public class Symbol {
 		} else {
 			throw new Exception("NonTerminalSymbol needs to be a single character");
 		}
+	}
+	
+	/*
+	 * Find the FIRST of the symbol
+	 * */
+	private void calculateFirst() {
+		first.clear();
+		if (type == Type.TERMINAL) {
+			first.add(this);
+		} else {
+			for (Production P: productionsTo) {
+				for (Symbol s: P.getDestiny()) {
+					if (s.getType() == Type.TERMINAL) {
+						first.add(s);
+						break;
+					} else {
+						Set<Symbol> firstY = s.getFirst();
+						first.addAll(firstY);
+						
+						Boolean existEpsilon = false;
+						for (Symbol tmp: firstY) {
+							if (tmp.toString().equals("&")) {
+								existEpsilon = true;
+								break;
+							}
+						}
+						
+						if (!existEpsilon) {
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	/*
+	 * Returns the FIRST of the symbol
+	 * */
+	public Set<Symbol> getFirst() {
+		calculateFirst();
+		return first;
+	}
+	
+	/*
+	 * Find the FOLLOW of the symbol
+	 * */
+	private void calculateFollow() {
+		
+	}
+	
+	/*
+	 * Returns the FOLLOW of the symbol
+	 * */
+	public Set<Symbol> getFollow() {
+		calculateFollow();
+		return follow;
 	}
 	
 	/*
